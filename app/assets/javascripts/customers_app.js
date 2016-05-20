e861e0304689472b7b740f680c77846f4412a91d
Josh Hunter <josh-hunter@am.com>
2016-05-20 15:21:57 -0500

    moved customer details to a separate view

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,32 +1,62 @@
-var app = angular.module('customers', [])
-  .controller('CustomerSearchController', ['$scope', '$http', function CustomerSearchController($scope, $http) {
-    var page = 0;
+var app = angular.module('customers', ['ngRoute', 'templates']);
 
-    $scope.customers = [];
-    $scope.search = function(searchTerm) {
-      if (searchTerm.length < 3) {
-        return;
-      }
-      $http.get("/customers.json",
-        { "params": { "keywords": searchTerm, "page": page } } 
-      ).then(function(response) {
-        $scope.customers = response.data;
-      }, function(response) {
-          alert("There was a problem: " + response.status);
-        }
-      );
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    controller: "CustomerSearchController",
+    templateUrl: "customer_search.html"
+  }).when('/:id', {
+    controller: "CustomerDetailController",
+    templateUrl: "customer_detail.html"
+  });
+}]);
+
+app.controller('CustomerSearchController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+  var page = 0;
+
+  $scope.customers = [];
+  $scope.search = function(searchTerm) {
+    if (searchTerm.length < 3) {
+      return;
     }
-
-    $scope.previousPage = function() {
-      if (page > 0) {
-        page = page - 1;
-        $scope.search($scope.keywords);
+    $http.get("/customers.json",
+      { "params": { "keywords": searchTerm, "page": page } } 
+    ).then(function(response) {
+      $scope.customers = response.data;
+    }, function(response) {
+        alert("There was a problem: " + response.status);
       }
-    }
+    );
+  }
 
-    $scope.nextPage = function() {
-      page = page + 1;
+  $scope.previousPage = function() {
+    if (page > 0) {
+      page = page - 1;
       $scope.search($scope.keywords);
     }
-  }])
-;
+  }
+
+  $scope.nextPage = function() {
+    page = page + 1;
+    $scope.search($scope.keywords);
+  }
+
+  $scope.viewDetails = function(customer) {
+    $location.path("/" + customer.id);
+  }
+}]);
+
+app.controller("CustomerDetailController", [ "$scope","$http","$routeParams", function($scope , $http , $routeParams) {
+
+  var customerId = $routeParams.id;
+  $scope.customer = {};
+
+  $http.get(
+    "/customers/" + customerId + ".json"
+  ).then(function(response) {
+      $scope.customer = response.data;
+    },function(response) {
+      alert("There was a problem: " + response.status);
+    }
+  );
+}]);
+
